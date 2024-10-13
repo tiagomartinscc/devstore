7aebaa02c6342a76c30ae11e5c1f83d86958a82a
Tiago Martins <tiagomartinscc@hotmail.com>
2024-10-13 20:19:29 -0300

    add Header layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    fontFamily: {},
+    gridTemplateRows: {
+      app: 'min-content max-content',
+    },
+  },
   plugins: [],
-};
-export default config;
+}
+export default config
