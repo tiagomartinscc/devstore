0a070f8be991a856a80fea5108f5ed98d8710f89
Tiago Martins <tiagomartinscc@hotmail.com>
2024-10-14 21:49:10 -0300

    add to cart

---
+++ src/components/add-to-cart-button.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useCart } from '@/context/cart-context'
+
+export interface AddToCartButtonProps {
+  productId: number
+}
+
+export function AddToCartButton({ productId }: AddToCartButtonProps) {
+  const { addToCart } = useCart()
+  return (
+    <button
+      onClick={() => addToCart(productId)}
+      type="button"
+      className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
+    >
+      Adicionar ao carrinho
+    </button>
+  )
+}
